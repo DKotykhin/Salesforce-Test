2bc616bf6e080b04191c45c25e7638438f8e1273
Dmytro <kotykhin_d@ukr.net>
2023-04-18 14:03:08 +0200

    delivery first complete

--- force-app/main/default/aura/deliveryComponent/deliveryComponentController.js
+++ force-app/main/default/aura/deliveryComponent/deliveryComponentController.js
@@ -1,12 +1,8 @@
 ({
-    handleChangeName: function (component, event, helper) {
-        helper.switchImage(component, event)
-    },
-
     doInit: function (component, event, helper) {
         // Prepare a new record from template
         component.find("deliveryRecordCreator").getNewRecord(
-            "DeliveryList__c", // sObject type (objectApiName)
+            "Delivery__c", // sObject type (objectApiName)
             null,      // recordTypeId
             false,     // skip cache?
             $A.getCallback(function () {
@@ -17,29 +13,54 @@
                     return;
                 }
                 console.log("Record template initialized: " + rec.apiName);
+                component.find("itemFieldQuantity").set("v.value", 1);
             })
         );
     },
 
-    handleSaveDelivery: function (component, event, helper) {
-        let newDelivery = component.get("v.newDelivery");
-        // newDelivery = component.get("v.deliveryFields");
-        newDelivery.Contact__c = component.get("v.recordId");
-        component.set("v.newDelivery", component.get("v.deliveryFields"));
-        console.log(newDelivery);
-        console.log(component.get("v.deliveryFields"));
-        console.log(component.get("v.recordId"));
+    handleChangeName: function (component, event, helper) {
+        helper.switchImage(component, event)
+    },
 
-        component.find("deliveryRecordCreator").saveRecord(function (saveResult) {
-            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-                console.log('record is saved successfully');
-            } else if (saveResult.state === "INCOMPLETE") {
-                console.log("User is offline, device doesn't support drafts.");
-            } else if (saveResult.state === "ERROR") {
-                console.log('Problem saving record, error: ' + JSON.stringify(saveResult.error));
-            } else {
-                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
-            }
-        });
-    }
+    handleChangeWeight: function (component, event, helper) {
+        helper.switchWeight(component, event);
+    },
+
+    handleChangeQuantity: function (component, event, helper) {
+        helper.switchQuantity(component, event);
+    },
+
+    handleSaveDelivery: function (component, event, helper) {
+        component.set("v.deliveryFields.Contact__c", component.get("v.recordId"));
+
+        const totalPrice = component.get("v.totalPrice");
+        component.set("v.deliveryFields.Price__c", totalPrice);
+
+        const isValidate = helper.validationMethod(component);
+
+        if (isValidate) {
+            component.find("deliveryRecordCreator").saveRecord(function (saveResult) {
+                if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                    console.log('record is saved successfully');
+                    $A.get("e.force:closeQuickAction").fire()
+                    component.find('notifLib').showToast({
+                        "title": "Record saving",
+                        "message": 'Record is saved successfully!',
+                        "variant": "success"
+                    });
+                } else if (saveResult.state === "INCOMPLETE") {
+                    console.log("User is offline, device doesn't support drafts.");
+                } else if (saveResult.state === "ERROR") {
+                    console.log('Problem saving record, error: ' + JSON.stringify(saveResult.error));
+                    component.find('notifLib').showToast({
+                        "title": "Something has gone wrong!",
+                        "message": JSON.stringify(saveResult.error),
+                        "variant": "error"
+                    });
+                } else {
+                    console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+                }
+            });
+        }
+    },
 })
--- force-app/main/default/aura/deliveryComponent/deliveryComponentHelper.js
+++ force-app/main/default/aura/deliveryComponent/deliveryComponentHelper.js
@@ -2,19 +2,99 @@
     switchImage: function (component, event) {
         const itemValue = event.getParam("value");
         let profUrl;
+        let itemPrice = 0;
         switch (itemValue) {
-            case "Honduras": profUrl = $A.get('$Resource.Coffee_Honduras');
+            case "Honduras":
+                profUrl = $A.get('$Resource.Coffee_Honduras');
+                itemPrice = 220;
                 break;
-            case "Brazil": profUrl = $A.get('$Resource.Coffee_Brazil');
+            case "Brazil":
+                profUrl = $A.get('$Resource.Coffee_Brazil');
+                itemPrice = 190;
                 break;
-            case "Guatemala": profUrl = $A.get('$Resource.Coffee_Guatemala');
+            case "Guatemala":
+                profUrl = $A.get('$Resource.Coffee_Guatemala');
+                itemPrice = 250;
                 break;
-            case "Ethiopia": profUrl = $A.get('$Resource.Coffee_Ethiopia');
+            case "Ethiopia":
+                profUrl = $A.get('$Resource.Coffee_Ethiopia');
+                itemPrice = 300;
                 break;
-            case "Colombia": profUrl = $A.get('$Resource.Coffee_Colombia');
+            case "Colombia":
+                profUrl = $A.get('$Resource.Coffee_Colombia');
+                itemPrice = 330;
                 break;
-            default: profUrl = $A.get('$Resource.Coffee_Honduras');
+            default:
+                profUrl = $A.get('$Resource.Coffee_Honduras');
+                itemPrice = 0;
+        }
+        component.set("v.itemPrice", itemPrice);
+        const quantityValue = component.find("itemFieldQuantity").get("v.value");
+        const weightValue = component.find("itemFieldWeight").get("v.value");
+        const totalPrice = itemPrice * weightValue * quantityValue;
+        if (totalPrice) {
+            component.set("v.totalPrice", totalPrice);
+        } else {
+            component.set("v.totalPrice", itemPrice);
         }
         component.set("v.image", profUrl);
+    },
+
+    switchWeight: function (component, event) {
+        const weightValue = +event.getParam("value");
+        const itemPrice = component.get("v.itemPrice");
+        const quantityValue = component.find("itemFieldQuantity").get("v.value");
+        const totalPrice = itemPrice * weightValue * quantityValue;
+        component.set("v.totalPrice", totalPrice);
+    },
+
+    switchQuantity: function (component, event) {
+        const quantityValue = event.getParam("value");
+        const itemPrice = component.get("v.itemPrice");
+        const weightValue = component.find("itemFieldWeight").get("v.value");
+        const totalPrice = itemPrice * weightValue * quantityValue;
+        component.set("v.totalPrice", totalPrice);
+    },
+
+    validationMethod: function (component) {
+        const nameField = component.find('itemFieldName');
+        const nameFieldValue = nameField.get('v.value');
+        const roastField = component.find('itemFieldRoast');
+        const roastFieldValue = roastField.get('v.value');
+        const weightField = component.find('itemFieldWeight');
+        const weightFieldValue = weightField.get('v.value');
+        const addressField = component.find('itemFieldAddress');
+        const addressFieldValue = addressField.get('v.value');
+        if (!nameFieldValue) {
+            component.find('notifLib').showToast({
+                "title": "Form warn:",
+                "message": 'Choose the item!',
+                "variant": "warning"
+            });
+            return false
+        } else if (!roastFieldValue) {
+            component.find('notifLib').showToast({
+                "title": "Form warn:",
+                "message": 'Choose the roast!',
+                "variant": "warning"
+            });
+            return false
+        } else if (!weightFieldValue) {
+            component.find('notifLib').showToast({
+                "title": "Form warn:",
+                "message": 'Choose the weight!',
+                "variant": "warning"
+            });
+            return false
+        } else if (!addressFieldValue) {
+            component.find('notifLib').showToast({
+                "title": "Form warn:",
+                "message": 'Fill the address field!',
+                "variant": "warning"
+            });
+            return false
+        } else {
+            return true
+        }
     },
 })
